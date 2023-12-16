5f08a02bd8a7cc0adda814a4d68d5c79a2a2555d
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-12-17 03:20:50 +0800

    counter

---
+++ src/multi-thread/counter.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX 50
+
+int number;
+pthread_mutex_t mtx;
+
+void* funcA_num(void* arg) {
+    for (int i = 0; i < MAX; ++i) {
+        pthread_mutex_lock(&mtx);
+        int cur = number;
+        cur++;
+        number = cur;
+        printf("A ===id = %lu, number = %d\n", pthread_self(), number);
+        pthread_mutex_unlock(&mtx);
+        // usleep(10);
+    }
+
+    return NULL;
+}
+
+void* funcB_num(void* arg) {
+    for (int i = 0; i < MAX; ++i) {
+        pthread_mutex_lock(&mtx);
+        int cur = number;
+        cur++;
+        number = cur;
+        printf("                   B ===id = %lu, number = %d\n", pthread_self(), number);
+        pthread_mutex_unlock(&mtx);
+    }
+
+    return NULL;
+}
+
+int main(void) {
+    pthread_t p1, p2;
+    pthread_mutex_init(&mtx, NULL);
+
+    pthread_create(&p1, NULL, funcA_num, NULL);
+    pthread_create(&p2, NULL, funcB_num, NULL);
+
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+
+    pthread_mutex_destroy(&mtx);
+
+    return 0;
+}
--- src/multi-thread/mutex.cc
+++
@@ -1,7 +0,0 @@
-#include <pthread.h>
-
-int main(void) {
-    pthread_mutex_t mtx;
-
-    pthread_mutex_init(&mtx, NULL);
-}
