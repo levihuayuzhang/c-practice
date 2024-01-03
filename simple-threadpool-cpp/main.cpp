99a5ee5d896eefae9a7c3435044017af8729606b
Huayu Zhang <zhanghuayu.dev@gmail.com>
2024-01-03 23:26:47 +0800

    add unix file io test

--- simple-threadpool-cpp/main.cpp
+++ simple-threadpool-cpp/main.cpp
@@ -1,28 +1,24 @@
-#include <cstdio>
-#include <unistd.h>
-#include <cstdlib>
-#include <pthread.h>
-#include <iostream>
-#include "ThreadPool.h"
 #include "ThreadPool.cpp"
+#include "ThreadPool.h"
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
 
-void taskFunc(void *arg)
-{
-    int num = *(int *)arg;
-    std::cout << "Thread " << std::to_string((long long)pthread_self()) << " is working, number = " << num << std::endl;
-    sleep(1);
+void taskFunc(void *arg) {
+  int num = *(int *)arg;
+  std::cout << "Thread " << std::to_string((long long)pthread_self())
+            << " is working, number = " << num << "\n";
+  sleep(1);
 }
 
-int main(int argc, char const *argv[])
-{
-    ThreadPool<int> pool(3, 10);
-    for (int i = 0; i < 100; ++i)
-    {
-        int *num = new int(i+100);
-        pool.addTask(Task<int>(taskFunc, num));
-    }
+int main(int argc, char const *argv[]) {
+  ThreadPool<int> pool(3, 10);
+  for (int i = 0; i < 100; ++i) {
+    int *num = new int(i + 100);
+    pool.addTask(Task<int>(taskFunc, num));
+  }
 
-    sleep(20);
+  sleep(20);
 
-    return 0;
+  return 0;
 }
---
+++ socket/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <arpa/inet.h>
+// #include <sys/socket.h>
+
+int main(int argc, const char *argv[]) 
+{ 
+    std::cout << "this is socket\n"; 
+}
